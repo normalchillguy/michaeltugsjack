a1f64b788ba2155d0e4b148fcd862485e6790c89
normalchillguy <77762382+normalchillguy@users.noreply.github.com>
2025-06-19 22:55:13 -0500

    fix: Re-enable static export for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-  // output: 'export',
+  output: 'export',
   // distDir: 'dist',
   basePath: isProd ? '/michaeltugsjack' : '',
   images: {
